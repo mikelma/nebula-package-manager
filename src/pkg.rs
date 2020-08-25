6e7b34b382982f6c210216c638f43bc1590690f4
mikelma <mikelma7@gmail.com>
2020-08-25 15:59:19 +0200

    PkgSource added to package definition

--- src/pkg.rs
+++ src/pkg.rs
@@ -1,42 +1,48 @@
-use crate::RepoType;
 use serde_derive::{Deserialize, Serialize};
 // use version_compare::version::Version;
+use crate::RepoType;
 
 #[derive(Deserialize, Serialize, Debug, PartialEq)]
 pub struct Package {
     name: String,
     version: Option<String>,
-    #[serde(rename = "repository")]
-    from: Option<RepoType>,
+    #[serde(rename = "source")]
+    source: Option<PkgSource>,
     #[serde(rename = "dependencies")]
     depends: Option<Vec<Dependency>>,
 }
+
+#[derive(Deserialize, Serialize, Debug, PartialEq)]
+pub struct PkgSource(RepoType, String);
 
 #[derive(Deserialize, Serialize, Debug, PartialEq)]
 pub struct Dependency(String, String);
 
 #[cfg(test)]
 mod tests {
-    use crate::pkg::{Dependency, Package};
-    use crate::repos::RepoType;
+    use crate::pkg::{Dependency, Package, PkgSource};
+    use crate::RepoType;
     use toml;
     #[test]
     fn package_seralization_deserialization() {
         let package = Package {
             name: "proba".to_string(),
             version: Some("1.2.3".to_string()),
-            from: Some(RepoType::Nebula),
+            source: Some(PkgSource(
+                RepoType::Nebula,
+                "source.url.eus/proba".to_string(),
+            )),
             depends: Some(vec![
                 Dependency("dep1".to_string(), "3.1".to_string()),
                 Dependency("dep2".to_string(), "".to_string()),
             ]),
         };
 
-        let _pkg_str_ser = toml::to_string(&package).unwrap();
+        let pkg_str_ser = toml::to_string(&package).unwrap();
         let pkg_str = r#"
         name = 'proba'
         version = '1.2.3'
-        repository = 'nebula'
+        source = ['nebula', 'source.url.eus/proba']
         dependencies = [['dep1', '3.1'], ['dep2', '']]
         "#;
 
