cc7f49b1f57a7993242d699c10ecf601d8e8f8a2
mikelma <mikelma7@gmail.com>
2020-08-26 13:41:25 +0200

    implementations of Package, PkgSource and Depency and updated depends field of Package

--- src/pkg.rs
+++ src/pkg.rs
@@ -9,14 +9,58 @@ pub struct Package {
     #[serde(rename = "source")]
     source: Option<PkgSource>,
     #[serde(rename = "dependencies")]
-    depends: Option<Vec<Dependency>>,
+    depends: Option<Vec<Vec<Dependency>>>,
+}
+
+impl Package {
+    pub fn new(name: &str) -> Package {
+        Package {
+            name: name.to_string(),
+            version: None,
+            source: None,
+            depends: None,
+        }
+    }
+
+    pub fn set_version(&mut self, ver: &str) {
+        self.version = Some(ver.to_string());
+    }
+
+    pub fn set_source(&mut self, repo_type: RepoType, url: &str) {
+        self.source = Some(PkgSource(repo_type, url.to_string()));
+    }
+
+    pub fn add_dependency(&mut self, deps: Vec<Dependency>) {
+        if let Some(vec) = &mut self.depends {
+            vec.push(deps);
+        } else {
+            self.depends = Some(vec![deps]);
+        }
+    }
 }
 
 #[derive(Deserialize, Serialize, Debug, PartialEq)]
 pub struct PkgSource(RepoType, String);
 
+impl PkgSource {
+    pub fn from(repo_type: RepoType, url: &str) -> PkgSource {
+        PkgSource(repo_type, url.to_string())
+    }
+}
+
 #[derive(Deserialize, Serialize, Debug, PartialEq)]
 pub struct Dependency(String, String);
+
+impl Dependency {
+    /// Creates a new `Dependency` given the name and version requirement. If there is no version
+    /// requirement, `version_req` parameter must be `None`.
+    pub fn from(name: &str, version_req: Option<&str>) -> Dependency {
+        match version_req {
+            Some(v) => Dependency(name.to_string(), v.to_string()),
+            None => Dependency(name.to_string(), "".to_string()),
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -33,17 +77,21 @@ mod tests {
                 "source.url.eus/proba".to_string(),
             )),
             depends: Some(vec![
-                Dependency("dep1".to_string(), "3.1".to_string()),
-                Dependency("dep2".to_string(), "".to_string()),
+                vec![Dependency("dep1".to_string(), "3.1".to_string())],
+                vec![
+                    Dependency("dep2".to_string(), "".to_string()),
+                    Dependency("dep3".to_string(), "5.1".to_string()),
+                ],
             ]),
         };
-
         let pkg_str_ser = toml::to_string(&package).unwrap();
+        println!("{}", pkg_str_ser);
+
         let pkg_str = r#"
         name = 'proba'
         version = '1.2.3'
         source = ['nebula', 'source.url.eus/proba']
-        dependencies = [['dep1', '3.1'], ['dep2', '']]
+        dependencies = [[['dep1', '3.1']], [['dep2', ''], ['dep3', '5.1']]]
         "#;
 
         let pkg_de: Package = toml::from_str(&pkg_str).unwrap();
