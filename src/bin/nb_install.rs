4dc7eed0565b2813d6743821ad7e99a93cd5c9de
mikelma <mikelma7@gmail.com>
2020-08-26 15:05:16 +0200

    cleanup

--- src/bin/nb_install.rs
+++ src/bin/nb_install.rs
@@ -1,52 +1,16 @@
 extern crate regex;
 
-use regex::Regex;
-use std::fs::{read_to_string, File};
-use std::io::{BufRead, BufReader, Seek, SeekFrom};
-use std::path::Path;
+use nbpm::Repository;
 
-fn main() -> () {
-    // let file = read_to_string("/tmp/nebula/repo/debian/Packages-main").unwrap();
-    let mut buff = BufReader::new(File::open("/tmp/nebula/repo/debian/Packages-main").unwrap());
-
-    let re = Regex::new(r"^Package: calcurse").unwrap();
-    /*
-    buff.lines()
-        .enumerate()
-        .filter(|tuple| tuple.1.is_ok()) // get line if not error
-        .filter(|tuple| re.is_match(tuple.1.as_ref().unwrap().as_str())) // get matches
-        .for_each(|(n, x)| {
-            println!(
-                "line {} seek {}: {}",
-                n,
-                buff.seek(SeekFrom::Current(0)).unwrap(),
-                x.unwrap()
-            )
-        });
-    */
-    let mut readnext = false;
-    let mut matches = vec![];
-    for line in buff.lines() {
-        let mut line = line.unwrap().trim_end().to_string();
-        if re.is_match(&line) {
-            readnext = true;
-            line.push('\n');
-            matches.push(line);
-            continue;
+fn main() {
+    let repos = nbpm::repos::create_repos().unwrap();
+    nbpm::initialize(&repos).unwrap();
+    // let mut matches = vec![];
+    for repo in repos {
+        let match_ = repo.search("libc", None).unwrap();
+        for m in match_.unwrap() {
+            println!("{:#?}", m);
         }
-        if !line.is_empty() && readnext {
-            println!("line: {}", line);
-            if let Some(last_match) = matches.last_mut() {
-                line.push('\n');
-                last_match.push_str(&line);
-            }
-        } else if line.is_empty() {
-            readnext = false;
-        }
-    }
-
-    for m in matches {
-        println!("-- match --");
-        m.split('\n').for_each(|a| println!("line: {}", a));
+        // matches.append(&mut match_.unwrap());
     }
 }
