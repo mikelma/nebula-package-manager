5d31b2fffe5eaa10bc19cb7770f5f5416a570cde
mikelma <mikelma7@gmail.com>
2020-10-06 09:45:53 +0200

    nebula repo index as lazy static

--- src/repos/nebula.rs
+++ src/repos/nebula.rs
@@ -1,10 +1,9 @@
 use serde_derive::Deserialize;
-use version_compare::{CompOp, Version};
-
 use std::fs;
 use std::path::Path;
 use std::path::PathBuf;
 use std::process::exit;
+use version_compare::{CompOp, Version};
 
 use crate::pkgdb::PkgDB;
 use crate::{pkg, utils, NebulaError, Package, RepoType, Repository, CONFIG};
@@ -14,28 +13,45 @@ pub struct NebulaConfig {
     pub repository: String,
 }
 
-const PKG_INDEX_NAME: &'static str = "packages.toml";
+/// Name of the directory inside nbpm's home that contains all
+/// info about nebula's repository
+const NB_REPO_DIR: &'static str = "nebula";
+/// Name of the file that contains the repository index of a nebula repository
+const NB_PKG_INDEX_NAME: &'static str = "packages.toml";
+
 lazy_static! {
     /// Nebula's repository index
-    pub static ref NB_REPO_INDEX: PkgDB = match PkgDB::from(Path::new(PKG_INDEX_NAME).as_ref()) {
-        Ok(v) => v,
-        Err(e) => {
-            eprintln!("Fatal error: {:?}", e);
-            exit(1);
-        },
+    pub static ref NB_REPO_INDEX: PkgDB = {
+        let filepath = Path::new(CONFIG.repos_dir()).join(NB_REPO_DIR).join(NB_PKG_INDEX_NAME);
+        match PkgDB::from(&filepath) {
+            Ok(v) => v,
+            Err(e) => {
+                eprintln!("Fatal error loading nebula repo index: {:?} {}", e, filepath.display());
+                exit(1);
+            },
+        }
     };
 }
 
 pub struct RepoNebula<'d> {
+    /// path of directory where all files relative to the nebula repository are
     repo_dir: PathBuf,
+    /// reference to the nebula repo configuration (NebulaConfig) inside the main configuration
+    /// (CONFIG object)
     conf: &'d NebulaConfig,
 }
 
 impl<'d> RepoNebula<'d> {
     pub fn new() -> Result<RepoNebula<'d>, NebulaError> {
         let conf = &CONFIG.repos().nebula();
-        let repo_dir = CONFIG.repos_dir().join("nebula");
+        let repo_dir = CONFIG.repos_dir().join(NB_REPO_DIR);
+        // atributte index takes None as initial value, when the value is first called via index
+        // method, the atributte will take the PkgDB value (lazy loading).
         Ok(RepoNebula { conf, repo_dir })
+    }
+
+    pub fn repo_dir(&self) -> &Path {
+        self.repo_dir.as_path()
     }
 }
 
@@ -68,8 +84,8 @@ impl<'d> Repository for RepoNebula<'d> {
 
         info!("Downloading nebula's packages file...");
         utils::download(
-            format!("{}/{}", self.conf.repository, PKG_INDEX_NAME),
-            &self.repo_dir.join(PKG_INDEX_NAME),
+            format!("{}/{}", self.conf.repository, NB_PKG_INDEX_NAME),
+            &self.repo_dir.join(NB_PKG_INDEX_NAME),
         );
         Ok(())
     }
